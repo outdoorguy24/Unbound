7dea70f42e3e0603087c84c6b10d0d331e6e0be6
Alex Starr <youremail@example.com>
2025-06-04 08:37:21 -0500

    updated onboarding flow sequence

--- app/(onboarding)/_layout.tsx
+++ app/(onboarding)/_layout.tsx
@@ -3,12 +3,17 @@ import { Stack } from 'expo-router';
 export default function OnboardingLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="paywall" options={{ headerShown: false }} />
-      <Stack.Screen name="payment" options={{ headerShown: false }} />
-      <Stack.Screen name="signup" options={{ headerShown: false }} />
+      <Stack.Screen name="Screen1" options={{ headerShown: false }} />
+      <Stack.Screen name="Screen2" options={{ headerShown: false }} />
+      <Stack.Screen name="Screen3" options={{ headerShown: false }} />
+      <Stack.Screen name="Screen4" options={{ headerShown: false }} />
       <Stack.Screen name="Screen5" options={{ headerShown: false }} />
-      <Stack.Screen name="Screen10" options={{ headerShown: false }} />
       <Stack.Screen name="Screen6" options={{ headerShown: false }} />
+      <Stack.Screen name="Screen7" options={{ headerShown: false }} />
+      <Stack.Screen name="Screen10" options={{ headerShown: false }} />
+      <Stack.Screen name="Screen8" options={{ headerShown: false }} />
+      <Stack.Screen name="paywall-pricing" options={{ headerShown: false }} />
+      <Stack.Screen name="signup" options={{ headerShown: false }} />
       <Stack.Screen name="Screen9" options={{ headerShown: false }} />
       <Stack.Screen name="login" options={{ headerShown: false }} />
     </Stack>
